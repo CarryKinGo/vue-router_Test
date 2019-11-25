b1fc93e7a2bafdad401666c90f4cd00d187e8bbf
k00379218 <kongxiao1@huawei.com>
2019-11-25 14:09:31 +0800

    Description:
    APPx:
    Feature or Bugfix:

---
+++ src/components/details.vue
@@ -0,0 +1,12 @@
+<template id="details">
+			<div>
+				<h3>我是用户详细</h3>
+				<h3>当前完整路径fullPath:{{$route.fullPath}}</h3>
+				<h3>当前路径path:{{$route.path}}</h3>
+				<h3>当前参数params:{{$route.params}}</h3>
+				<h3>当前查询参数query:{{$route.query}}</h3>
+				<h3>hash:{{$route.hash}}</h3>
+				<h3>router:{{$route.router}}</h3>
+				<!--<h3>matched:{{$route.matched}}</h3>-->
+			</div>
+		</template>
---
+++ src/components/home.vue
@@ -0,0 +1,5 @@
+<template id="home">
+  <div>
+    <h3>我是主页</h3>
+    </div>
+    </template>
